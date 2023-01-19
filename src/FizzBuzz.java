497f1e848dd3d7caf248da1d6d7b970095a2b02a
Satsuki Kasiwagi <122766543+march2226@users.noreply.github.com>
2023-01-19 13:09:39 +0900

    課題提出

---
+++ src/FizzBuzz.java
@@ -0,0 +1,17 @@
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		for (int a = 1; a <= 100; a++) {
+			if (a % 3 == 0 && a % 5 == 0) 
+				System.out.println("FizzBuzz");
+			else if (a % 3 == 0) 
+				System.out.println("Fizz");
+			else if (a % 5 == 0) 
+				System.out.println("Buzz");
+			else {
+				// 課題提出
+				System.out.println(a);
+			}
+		}
+	}
+}
